73992b8d4b3eeb43ac61a4185fdc16c9596e7469
Julian <juliangarciapetcov16@gmail.com>
2026-02-16 17:43:46 +0100

    a

---
+++ src/Entidades/Combate.java
@@ -0,0 +1,51 @@
+package Entidades;
+
+import java.util.ArrayList;
+
+public class Combate {
+
+    private int turno;
+    private Sala sala;;
+    private ArrayList<Heroe> heroes;
+
+    //Constructor
+    public Combate(int turno, Sala sala) {
+        this.turno = turno;
+        this.sala = sala;
+        this.heroes = new ArrayList<>();
+    }
+
+    //Getter y setter
+    public int getTurno() {
+        return turno;
+    }
+
+    public void setTurno(int turno) {
+        this.turno = turno;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+    public ArrayList<Heroe> getHeroes() {
+        return heroes;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Combate{");
+        sb.append("turno=").append(turno);
+        sb.append(", sala=").append(sala);
+        sb.append(", heroes=").append(heroes);
+        sb.append('}');
+        return sb.toString();
+    }
+
+
+}

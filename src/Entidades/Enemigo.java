46f4285b32349866d7d7c3e787f89a66195d4158
miriangrr <miriangonzrod26@gmail.com>
2026-02-16 17:09:32 +0100

    a

--- src/Entidades/Enemigo.java
+++ src/Entidades/Enemigo.java
@@ -2,9 +2,15 @@ package Entidades;
 
 public class Enemigo extends Personaje {
     protected TipoEnemigo tipo;
-    protected int expOtorgada;
+    protected int expOtorgada; //(experiencia que da al morir)
 
-    public Enemigo(String nombre, int puntosVidaMax, int puntosVidaActual, int ataque, int defensa, boolean vivo) {
+    public Enemigo(String nombre, int puntosVidaMax, int puntosVidaActual, int ataque, int defensa, boolean vivo, int expOtorgada, TipoEnemigo tipo) {
         super(nombre, puntosVidaMax, puntosVidaActual, ataque, defensa, vivo);
+        this.expOtorgada = expOtorgada;
+        this.tipo = tipo;
     }
-}
+
+
+
+    }
+

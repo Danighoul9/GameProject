3d908ee0a44f683c167b9b33f08b07b4285277e1
miriangrr <miriangonzrod26@gmail.com>
2026-02-16 17:46:35 +0100

    a

--- src/Entidades/Enemigo.java
+++ src/Entidades/Enemigo.java
@@ -33,17 +33,62 @@ public class Enemigo extends Personaje {
 
     }
 
-    //void usarHabilidadEspecial(Personaje objetivo) :
+    public int getExpOtorgada() {
+        return expOtorgada;
+    }
+
+    public Enemigo setExpOtorgada(int expOtorgada) {
+        this.expOtorgada = expOtorgada;
+        return this;
+    }
+
+    public TipoEnemigo getTipo() {
+        return tipo;
+    }
+
+    public Enemigo setTipo(TipoEnemigo tipo) {
+        this.tipo = tipo;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Enemigo{");
+        sb.append("expOtorgada=").append(expOtorgada);
+        sb.append(", tipo=").append(tipo);
+        sb.append(", ataque=").append(ataque);
+        sb.append(", defensa=").append(defensa);
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append(", puntosVidaActual=").append(puntosVidaActual);
+        sb.append(", puntosVidaMax=").append(puntosVidaMax);
+        sb.append(", vivo=").append(vivo);
+        sb.append('}');
+        return sb.toString();
+    }
+//void usarHabilidadEspecial(Personaje objetivo) :
     //GOBLIN: "Golpe Rápido" - ataca dos veces seguidas con daño
     //reducido
     //ORCO: "Grito de Guerra" - aumenta su ataque temporalmente
     //DRAGON: "Aliento de Fuego" - daña a todos los héroes
 
-    void usarHabilidadEspecial(Personaje objetivo){
+    public void usarHabilidadEspecial(Personaje objetivo){
+    if (this.tipo==TipoEnemigo.GOBLIN) {
+        System.out.println("Golpe Rapido");
+        atacar(objetivo);
+        atacar(objetivo);
+    }
+        if (this.tipo==TipoEnemigo.ORCO){
+            System.out.println("Grito guerra");
+            this.ataque *= 1.2;
+
+        }
+        if (this.tipo==TipoEnemigo.DRAGON){
+            System.out.println("Aliento de Fuego");
+        }
 
     }
-}
 
+    }
 
 
 

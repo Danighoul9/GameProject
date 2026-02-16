14e75d080349d92762c4112ceb23ab86765ae3d9
Hugo2526 <hcayege0104@g.educaand.es>
2026-02-16 17:25:30 +0100

    a

--- src/Entidades/Heroe.java
+++ src/Entidades/Heroe.java
@@ -2,28 +2,27 @@ package Entidades;
 
 import java.util.ArrayList;
 
-public class Heroe {
+public class Heroe extends Personaje{
     private TipoHeroe tipo;
     int nivel = 1;
     int experiencia = 0;
     ArrayList<Item> item;
-/*
-if (this.tipo == TipoHeroe.GUERRERO){
 
-        } else if (this.tipo == TipoHeroe.ARQUERO) {
 
-        } else if (this.tipo = TipoHeroe.MAGO) {
-
-        }
-        this.item = new ArrayList<>();
- */
-    public Heroe(TipoHeroe tipo, ArrayList<Item> item) {
+    public Heroe(String nombre, boolean vivo, TipoHeroe tipo, ArrayList<Item> item) {
+        super(nombre, 0, 0, 0, vivo);
         if (this.tipo == TipoHeroe.GUERRERO){
-
+            this.puntosVidaActual = 100;
+            this.ataque = 20;
+            this.defensa = 15;
         } else if (this.tipo == TipoHeroe.ARQUERO) {
-            
+            this.puntosVidaActual = 60;
+            this.ataque = 30;
+            this.defensa = 5;
         } else if (this.tipo == TipoHeroe.MAGO) {
-
+            this.puntosVidaActual = 80;
+            this.ataque = 25;
+            this.defensa = 10;
         }
         this.item = new ArrayList<>();
     }
@@ -60,6 +59,23 @@ if (this.tipo == TipoHeroe.GUERRERO){
         this.tipo = tipo;
     }
 
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Heroe{");
+        sb.append("tipo=").append(tipo);
+        sb.append(", nivel=").append(nivel);
+        sb.append(", experiencia=").append(experiencia);
+        sb.append(", item=").append(item);
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append(", puntosVidaActual=").append(puntosVidaActual);
+        sb.append(", puntosVidaMax=").append(puntosVidaMax);
+        sb.append(", ataque=").append(ataque);
+        sb.append(", defensa=").append(defensa);
+        sb.append(", vivo=").append(vivo);
+        sb.append('}');
+        return sb.toString();
+    }
+
     /**
      * Habilidades Especiales de los heroes
      * @param objetivo
@@ -83,6 +99,19 @@ if (this.tipo == TipoHeroe.GUERRERO){
     public void ganarExperiencia(int exp){
         this.experiencia += exp;
     }
+
+    public void subirNivel(){
+        if (this.experiencia >= 100){
+            this.puntosVidaMax += 20;
+            this.ataque += 5;
+            this.defensa += 3;
+            this.experiencia = 0;
+        }
+    }
+
+    public void usarItem(Item item){
+    }
+
 
 
 }
